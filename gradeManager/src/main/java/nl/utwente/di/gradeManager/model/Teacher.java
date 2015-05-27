937d859f4ecfbdb33d28de0bb4a7d6605bebfd6a
KevinBooijink <Kevin@Kevin-Laptop>
2015-05-27 09:31:48 +0200

    Rest van de dingen afhankelijk van de person-klasse ook aangepast

--- gradeManager/src/main/java/nl/utwente/di/gradeManager/model/Teacher.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/model/Teacher.java
@@ -9,9 +9,10 @@ public class Teacher extends Person{
 	 * Constructs the teacher object.
 	 * @param argUsername the username of the teacher.
 	 */
-	public Teacher(String argUsername, String argPassword) {
-		super(argUsername,argPassword);
-		
+	public Teacher(int argPersonID, String argFirstname, String argSurname,
+			String argUsername, String argPassword) {
+		super(argPersonID, argFirstname, argSurname, argUsername, argPassword);
+		// TODO Auto-generated constructor stub
 	}
 
 	private List<Module> modules;
