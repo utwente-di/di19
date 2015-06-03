22df8f08f1ee0f3df6696abcb33d90a0c3e7877c
spzme <fgletharg@gmail.com>
2015-06-03 11:20:04 +0200

    -Overzicht + commentaar en javadoc

--- gradeManager/src/main/java/nl/utwente/di/gradeManager/debug/Debug.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/debug/Debug.java
@@ -4,17 +4,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * class for helping with debugging
+ * by making use of time. 
+ */
 public class Debug {
 
 	/**
 	 * Logs a message to be used for debugging.
 	 * @param out the message to be logged
 	 */	
-
-	
 	public static void logln(String out){
+		//generate a way of displaying the time
 		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 		Date date = new Date();
+		//display the time, aswell as the message.
 		System.out.println("Debug: " + "(" + dateFormat.format(date) + ") " + out);
 	}
 	
--- gradeManager/src/main/java/nl/utwente/di/gradeManager/pages/Hello.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/pages/Hello.java
@@ -6,17 +6,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import nl.utwente.di.gradeManager.debug.Debug;
 import nl.utwente.di.gradeManager.style.Style;
 
 @Path("/hello")
+/**
+ * Simple hello world page.
+ *
+ */
 public class Hello {
 
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
+	/**
+	 * Say hello to the user in the browser
+	 * @return a html page saying Hello!
+	 */
 	public String sayHtmlHello() {
 		int depth = 1;
-		System.out.println("A user accessed the hello world page!");
+		//log the access to the debug.
+		Debug.logln("Hello: A user accessed the hello world page!");
 		return "<html> <head> " + Style.generateCSSLink(depth) + "</head> <h1> Hello ! </h1> </html>";
 	}
 	
