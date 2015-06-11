cc318cc2f794e3f7c9451b283813a0e5069fe168
Tim de Klerk <t.a.c.deklerk@student.utwente.nl>
2015-06-11 11:16:34 +0200

    Beans en servlets aangemaakt

--- gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/Resulttable.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/Resulttable.java
@@ -1,6 +1,8 @@
 package nl.utwente.di.gradeManager.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,17 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import nl.utwente.di.gradeManager.model.Course;
+
 
 public class Resulttable extends HttpServlet {
 	
 	private final String jsp_address = "/WEB-INF/Student2.jsp";
-	private StudentCourses[] courses;
-	private String[] courseIDs;
-	
+	private Course[] courses;
 	
 	protected void setCourses(String[] courseIDs) {
-		this.courseIDs = courseIDs;
-		courses = new StudentCourses[courseIDs.length];
+		courses = new Course[courseIDs.length];
 		for (int i = 0; i < courses.length; i++) {
 			courses[i] = QueryvanFrank.getCourse(courseIDs[i]);
 		}
@@ -30,8 +31,20 @@ public class Resulttable extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		if (courses != null) {
+			List<Course> itemsList = new ArrayList<Course>();
+			for (int i = 0; i < courses.length; i++) {
+				if (courses[i] != null)
+					itemsList.add(courses[i]);
+			}
+			
+			String SID = request.getAttribute("studentID").toString();
+			StudentCourses bean = new StudentCourses(SID, itemsList);
+			request.setAttribute("coursestoShow", bean);
+		
 			RequestDispatcher dispatcher = request.getRequestDispatcher(jsp_address);
 			dispatcher.forward(request, response);
 			
 		}
+	}
 }
