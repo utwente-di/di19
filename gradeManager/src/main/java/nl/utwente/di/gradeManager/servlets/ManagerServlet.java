726dd858fdb27b0d390c048de170716fabed5017
spzme <fgletharg@gmail.com>
2015-07-03 12:20:46 +0200

    Protection of manager page by redirecting

--- gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/ManagerServlet.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/ManagerServlet.java
@@ -7,14 +7,45 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import nl.utwente.di.gradeManager.db.LoginDB;
 
 public class ManagerServlet extends HttpServlet{
 	
 	private final String jsp_address = "Manager.jsp";	
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException{
-		RequestDispatcher dispatcher = request.getRequestDispatcher(jsp_address);
-		dispatcher.forward(request, response);
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	
+	//protect this page from people who are not managers. 
+	HttpSession session = request.getSession(false);
+	System.out.println("manager servlet: doGet");
+	if(session == null){
+		//there is no session, so redirect to login page.
+		response.sendRedirect("Login.jsp");
+		return;
+	} else {
+		LoginDB loginDB = new LoginDB();
+		String loggedinpersonid = "";
+		try{
+		loggedinpersonid = loginDB.getLoggedInPersonid(session.getId());
+		} finally {
+			loginDB.closeConnection();
+		}
+		if (loggedinpersonid.equals("") ){
+			response.sendRedirect("Login.jsp");
+			return;
+		} else {
+			if(!loginDB.isManager(Integer.parseInt(loggedinpersonid.substring(1)))){
+				response.sendRedirect("Login.jsp");
+				return;
+			}
+		}
+		
+	}
+	
+	
+	RequestDispatcher dispatcher = request.getRequestDispatcher(jsp_address);
+	dispatcher.forward(request, response);
 	}
 }
