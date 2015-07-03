6da63ffa0f2c5f693627dc9170ff21b93c55acdd
Tim de Klerk <t.a.c.deklerk@student.utwente.nl>
2015-07-03 09:50:24 +0200

    Waarde van nieuwcijfer verbeterd: niet meer String->Double->BigDecimal maar gelijk Bigdecimal. Commentaar toegevoegd

--- gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/UpdateResult.java
+++ gradeManager/src/main/java/nl/utwente/di/gradeManager/servlets/UpdateResult.java
@@ -19,31 +19,44 @@ import nl.utwente.di.gradeManager.model.AssignmentResult;
 public class UpdateResult extends HttpServlet{
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		Double nieuwcijfer = Double.parseDouble(request.getParameter("nieuwcijfer"));
+		//StudentID uit de parameters halen
 		Integer studentid = Integer.parseInt(request.getParameter("studentid"));
+		//OccasionID ophalen
 		Integer occasionid = Integer.parseInt(request.getParameter("occasionid"));
+		//Een date object initialiseren
 		Date occasiondate = null;
+		//Het assignmentID uit de parameters halen
+		Integer assignmentid = Integer.parseInt(request.getParameter("assignmentid"));
+		//De String die uit de getParameter komt in het goede format in het date object zetten
 		try {
 			occasiondate = new SimpleDateFormat("YYYY-mm-dd").parse(request.getParameter("occasiondate"));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Integer assignmentid = Integer.parseInt(request.getParameter("assignmentid"));
+		//Parameter url ophalen, zodat deze servelt correct kan redirecten
 		String terugnaar = request.getParameter("terugnaar");
-		BigDecimal BDnieuwcijfer = new BigDecimal(nieuwcijfer);
+		//Het nieuwe cijfer in een BigDecimal zetten
+		BigDecimal BDnieuwcijfer = new BigDecimal(request.getParameter("nieuwcijfer"));
 		
+		//Nieuw assignmentResult object aanmaken om mee te geven aan de query voor de database
 		AssignmentResult nieuwresultaat = new AssignmentResult(occasionid, studentid, occasiondate, BDnieuwcijfer, assignmentid);
 		
-		Debug.logln("cijfer:" + nieuwcijfer + " sid:" + studentid + " ocid:" + occasionid  + " date:" + occasiondate);
+		Debug.logln("cijfer:" + BDnieuwcijfer + " sid:" + studentid + " ocid:" + occasionid  + " date:" + occasiondate);
+		
+		//Database verbinding opzetten
 		GradesDB gradesDB = new GradesDB();
 		try{
+			//Query voor het aanpassen van een resultaat uitvoeren
 			gradesDB.modifyAssignmentResult(occasionid, studentid, nieuwresultaat);
 		}finally{
+			//Verbinding sluiten
 			gradesDB.closeConnection();
 		}
 		
+		//Correcte opmaak van de doorstuur url maken
 		String url = "DocentTabel?" + terugnaar + "#Toets1";
+		//Terugsturen naar de module waar het cijfer in zit
 		response.sendRedirect(url);
 	}
 }
